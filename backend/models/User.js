e0b7e6b1bd0a2f73d8f39afa965c0242607530b9
Jbirkett831 <jbirkett831@gmail.com>
2025-04-15 17:25:38 -0700

    rremmoved backend from frontend

--- backend/models/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  passwordHash: { type: String, required: true },
-  displayName: { type: String, default: 'New User' }, // preset list
-}, { timestamps: true });
-
-module.exports = mongoose.model('User', UserSchema);
